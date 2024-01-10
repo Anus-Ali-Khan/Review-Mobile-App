96bf6fc1fc34e25ce0385355642f7d08590c20a4
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-01-10 13:05:46 +0500

    custom fonts added

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./screens/home";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return <Home />;
 }
